e2c45db303f11c4c2aa6fabf612c11ee239ddc2f
ItaloFelipe <felipe.italo@hotmail.com>
2020-02-02 13:19:44 -0300

    Adicionada transicao entre componentes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Header />
     <div class="row">
       <div class="col-xs-12 col-xl-12 mb-5">
-        <router-view />
+        <transition name="slide" mode="out-in">
+          <router-view />
+        </transition>
       </div>
     </div>
   </div>
@@ -40,5 +42,32 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
