b6e1cc80db7e7738adbab92d08a2b58e5f2073de
ItaloFelipe <felipe.italo@hotmail.com>
2020-02-02 17:33:01 -0300

    Configurado o Vue Resource (http Client)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from 'vue';
+
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stock-trader-34838.firebaseio.com/';
 Vue.filter('currency', (value) => {
 	return 'R$' + value.toLocaleString();
 });
